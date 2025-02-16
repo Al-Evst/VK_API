c58df1c1ae985ddcde4e06a60628d1366be50b11
Al-Evst <al.evst96@gmail.com>
2025-02-16 16:33:43 +0100

    Update vk_api.py

--- vk_api.py
+++ vk_api.py
@@ -4,11 +4,6 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
-token = os.getenv('VK_ACCESS_TOKEN')
-access_key = os.getenv('ACCESS_KEY')
-
 def shorten_link(token, link):
     api_url = 'https://api.vk.ru/method/utils.getShortLink'
     params = {
@@ -16,79 +11,96 @@ def shorten_link(token, link):
         'v': '5.199',
         'url': link
     }
+    
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
 
-    try:
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
+    response_data = response.json()
 
-        response_data = response.json()
+    if 'response' in response_data and 'short_url' in response_data['response']:
+        return response_data['response']['short_url']
+    else:
+        raise ValueError("Ошибка: не удалось сократить ссылку.")
 
-        if 'response' in response_data and 'short_url' in response_data['response']:
-            return response_data['response']['short_url']
-        else:
-            raise ValueError("Ошибка: не удалось сократить ссылку.")
 
-    except requests.exceptions.HTTPError as e:
-        return f"Ошибка HTTP: {e}"
-    except requests.exceptions.RequestException as e:
-        return f"Ошибка запроса: {e}"
-    except ValueError as e:
-        return str(e)
-    except Exception as e:
-        return f"Произошла непредвиденная ошибка: {e}"
-
-def count_clicks(token, short_url):
-    api_url = 'https://api.vk.ru/method/utils.getLinkStats' 
+def count_clicks(token, short_url,access_key):
+    api_url = 'https://api.vk.ru/method/utils.getLinkStats'
     params = {
         'access_token': token,
         'v': '5.199',
         'key': short_url.split('/')[3],
-        'access_key' : access_key,
-        'interval' : 'forever'
+        'access_key': access_key,
+        'interval': 'forever'
     }
 
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+
+    response_json = response.json()
+
+    if 'response' in response_json and 'views' in response_json['response']['stats'][0]:
+        clicks_count = response_json['response']['stats'][0]['views']
+        return clicks_count
+    else:
+        raise ValueError("Ошибка: не удалось получить количество кликов.")
+
+
+def is_shorten_link(token,url,access_key):
+    api_url = 'https://api.vk.ru/method/utils.getLinkStats'
     try:
+        params = {
+            'access_token': token,
+            'v': '5.199',
+            'key': url.split('/')[3],
+            'access_key': access_key,
+            'interval': 'forever'
+        }
+
+    
+
         response = requests.get(api_url, params=params)
         response.raise_for_status()
+        response_json = response.json()
 
-        response_data = response.json()
-        if 'response' in response_data and 'views' in response_data['response']['stats'][0]:
-            clicks_count = response_data['response']['stats'][0]['views']
-            return clicks_count
+        if 'response' in response_json and 'stats' in response_json['response']:
+            return True
         else:
-            raise ValueError("Ошибка: не удалось получить количество кликов.")
+            return False
+        
+    except IndexError:
+        return False
 
-    except requests.exceptions.HTTPError as e:
-        return f"Ошибка HTTP: {e}"
-    except requests.exceptions.RequestException as e:
-        return f"Ошибка запроса: {e}"
-    except ValueError as e:
-        return str(e)
-    except Exception as e:
-        return f"Произошла непредвиденная ошибка: {e}"
-
-def is_shorten_link(url):
+def handle_exception(func):
     
-    short_link_pattern = r'^https://vk\.cc/[a-zA-Z0-9]+$'
-    if re.match(short_link_pattern, url):
-        return True
-    return False
+    try:
+        return func()
+    except requests.exceptions.HTTPError as e:
+        print(f"Ошибка HTTP: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка запроса: {e}")
+    except ValueError as e:
+        print(str(e))
+    except Exception as e:
+        print(f"Произошла непредвиденная ошибка: {e}")
+
 
 def main():
+
+    load_dotenv() 
     
+    token = os.environ['VK_ACCESS_TOKEN']
+    access_key = os.environ['ACCESS_KEY']
+
     if not token or not access_key:
         print("Ошибка: токен или access_key не найдены.")
         return
 
     url = input("Введите ссылку для сокращения: ")
 
-   
-    if is_shorten_link(url):
-        clicks = count_clicks(token, url)
-        print('Количество кликов: ', clicks)
+    if is_shorten_link(token,url,access_key):
+        handle_exception(lambda: print('Количество кликов: ', count_clicks(token, url,access_key)))
     else:
-        shortened_url = shorten_link(token, url)
-        print('Сокращенная ссылка: ', shortened_url)
+        handle_exception(lambda: print('Сокращенная ссылка: ', shorten_link(token, url)))
 
 
 if __name__ == "__main__":
