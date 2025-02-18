dac9e43af820d336530233f2c1708287befa066e
Al-Evst <al.evst96@gmail.com>
2025-02-18 22:57:55 +0100

    Update vk_api.py

--- vk_api.py
+++ vk_api.py
@@ -11,120 +11,102 @@ def shorten_link(token, link):
         'v': '5.199',
         'url': link
     }
-    
-    try:
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
 
-        response_data = response.json()
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
 
-        if 'response' in response_data and 'short_url' in response_data['response']:
-            return response_data['response']['short_url']
+    response_data = response.json()
 
+    if 'response' in response_data and 'short_url' in response_data['response']:
+        return response_data['response']['short_url']
+    else:
         raise ValueError("Ошибка: не удалось сократить ссылку.")
-    except requests.exceptions.HTTPError as e:
-        print(f"Ошибка HTTP: {e}")
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка запроса: {e}")
-    except ValueError as e:
-        print(str(e))
-    except Exception as e:
-        print(f"Произошла непредвиденная ошибка: {e}")
-    
-    return None  
 
 
 def count_clicks(token, short_url, access_key):
     api_url = 'https://api.vk.ru/method/utils.getLinkStats'
-    
-    try:
-        parsed_url = urlparse(short_url)
-        key = parsed_url.path.split('/')[1]
 
-        params = {
-            'access_token': token,
-            'v': '5.199',
-            'key': key,
-            'access_key': access_key,
-            'interval': 'forever'
-        }
+    parsed_url = urlparse(short_url)
+    path_parts = parsed_url.path.split('/')
 
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
+    if len(path_parts) < 2:
+        raise ValueError("Ошибка: недействительная сокращенная ссылка.")
 
-        response_json = response.json()
+    key = path_parts[1]
 
-        if 'response' in response_json and 'views' in response_json['response']['stats'][0]:
-            clicks_count = response_json['response']['stats'][0]['views']
-            return clicks_count
+    params = {
+        'access_token': token,
+        'v': '5.199',
+        'key': key,
+        'access_key': access_key,
+        'interval': 'forever'
+    }
 
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+
+    response_json = response.json()
+
+    if 'response' in response_json and 'stats' in response_json['response'] and len(response_json['response']['stats']) > 0:
+        clicks_count = response_json['response']['stats'][0]['views']
+        return clicks_count
+    else:
         raise ValueError("Ошибка: не удалось получить количество кликов.")
-    
-    except requests.exceptions.HTTPError as e:
-        print(f"Ошибка HTTP: {e}")
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка запроса: {e}")
-    except ValueError as e:
-        print(str(e))
-    except Exception as e:
-        print(f"Произошла непредвиденная ошибка: {e}")
-    
-    return None  
 
 
 def is_shorten_link(token, url, access_key):
     api_url = 'https://api.vk.ru/method/utils.getLinkStats'
     
-    try:
-        parsed_url = urlparse(url)
-        key = parsed_url.path.split('/')[1]
+    parsed_url = urlparse(url)
+    path_parts = parsed_url.path.split('/')
 
-        params = {
-            'access_token': token,
-            'v': '5.199',
-            'key': key,
-            'access_key': access_key,
-            'interval': 'forever'
-        }
+    if len(path_parts) < 2:
+        return False  
 
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()
-        response_json = response.json()
+    key = path_parts[1]
 
-        return 'response' in response_json and 'stats' in response_json['response']
-        
-    except IndexError:
-        return False
-    except requests.exceptions.HTTPError as e:
-        print(f"Ошибка HTTP: {e}")
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка запроса: {e}")
-    except Exception as e:
-        print(f"Произошла непредвиденная ошибка: {e}")
-    
-    return False 
+    params = {
+        'access_token': token,
+        'v': '5.199',
+        'key': key,
+        'access_key': access_key,
+        'interval': 'forever'
+    }
+
+    response = requests.get(api_url, params=params)
+    response.raise_for_status()
+
+    response_json = response.json()
+
+    return 'response' in response_json and 'stats' in response_json['response']
 
 
 def main():
-    load_dotenv() 
-    
+    load_dotenv()
+
     token = os.environ['VK_ACCESS_TOKEN']
     access_key = os.environ['ACCESS_KEY']
 
-    if not token or not access_key:
+    if not (token and access_key):
         print("Ошибка: токен или access_key не найдены.")
         return
 
     url = input("Введите ссылку для сокращения: ")
 
-    if is_shorten_link(token, url, access_key):
-        clicks = count_clicks(token, url, access_key)
-        if clicks is not None:
-            print('Количество кликов: ', clicks)
-    else:
-        short_link = shorten_link(token, url)
-        if short_link is not None:
-            print('Сокращенная ссылка: ', short_link)
+    try:
+        if is_shorten_link(token, url, access_key):
+            clicks = count_clicks(token, url, access_key)  
+            print(f'Количество кликов: {clicks}')
+        else:
+            short_link = shorten_link(token, url)  
+            print(f'Сокращенная ссылка: {short_link}')
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка запроса: {e}")
+    except ValueError as e:
+        print(f"Ошибка значения: {e}")
+    except Exception as e:
+        print(f"Произошла непредвиденная ошибка: {e}")
 
 
 if __name__ == "__main__":
